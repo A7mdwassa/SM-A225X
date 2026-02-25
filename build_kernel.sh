a4ca0de0049f6ed610ff71ce3d6b72a1c1313c38
A7mdwassa <ahmdwassam112@gmail.com>
2026-02-25 15:40:00 +0200

    kernel: Fix Build

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+export CROSS_COMPILE=${HOME}/toolchains/gcc/bin/aarch64-linux-androidkernel-
+export CC=${HOME}/toolchains/clang-r383902b1/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 
@@ -11,4 +11,4 @@ export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a22_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+cp out/arch/arm64/boot/Image /mnt/hgfs/Firm/Image
